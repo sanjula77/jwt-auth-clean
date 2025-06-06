3650940aecdc184d2dc16880d733b1bde0047a24
Gihan sanjula <sanjulagihan94@gmail.com>
2025-06-06 21:29:41 +0530

    feat: implement change password and forgot password functionality with validation

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -36,3 +36,26 @@ exports.acceptCodeSchema = Joi.object({
 		}),
 	providedCode: Joi.number().required(),
 });
+
+exports.changePasswordSchema = Joi.object({
+	newPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+	oldPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+
+exports.acceptFPCodeSchema = Joi.object({
+	email: Joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: Joi.number().required(),
+	newPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-// const { identifier } = require('../middlewares/identification');
+const { identifier } = require('../middlewares/identification');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
-router.post('/signout', authController.signout);
+router.post('/signout', identifier, authController.signout);
 
-router.patch('/send-verification-code', authController.sendVerificationCode);
-router.patch('/verify-verification-code',authController.verifyVerificationCode);
-// router.patch('/change-password', identifier, authController.changePassword);
-// router.patch(
-// 	'/send-forgot-password-code',
-// 	authController.sendForgotPasswordCode
-// );
-// router.patch(
-// 	'/verify-forgot-password-code',
-// 	authController.verifyForgotPasswordCode
-// );
+router.patch('/send-verification-code', identifier, authController.sendVerificationCode);
+router.patch('/verify-verification-code', identifier, authController.verifyVerificationCode);
+router.patch('/change-password', identifier, authController.changePassword);
+router.patch('/send-forgot-password-code',authController.sendForgotPasswordCode);
+router.patch('/verify-forgot-password-code',authController.verifyForgotPasswordCode);
 
 module.exports = router;
